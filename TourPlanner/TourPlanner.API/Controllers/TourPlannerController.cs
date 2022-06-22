48b0f4a602566fddd2d0c75a4548cedc64ebdb00
Linus Dehner <if21b132@technikum-wien.at>
2022-06-22 22:01:23 +0200

    Added ASP .NET Web Api

---
+++ TourPlanner/TourPlanner.API/Controllers/TourPlannerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TourPlanner.Data;
+
+namespace TourPlannerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TourPlannerController : ControllerBase
+    {
+        private readonly ToursDataContext context;
+
+        public TourPlannerController(ToursDataContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Tours>>> Get()
+        {
+            return Ok(await this.context.Tours.ToListAsync());
+        }
+
+        [HttpGet("{TourId}")]
+        public async Task<ActionResult<Tours>> Get(Guid TourId)
+        {
+            var tour = await this.context.Tours.FindAsync(TourId);
+            if (tour == null) return BadRequest("Tour not found.");
+            return Ok(tour);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Tours>>> AddTour(Tours tour)
+        {
+            this.context.Tours.Add(tour);
+            await this.context.SaveChangesAsync();
+
+            return Ok(await this.context.Tours.ToListAsync());
+        }
+    }
+}
---
+++ TourPlanner/TourPlanner.API/Program.cs
@@ -0,0 +1,30 @@
+using TourPlanner.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ToursDataContext>(
+    o => o.UseNpgsql(builder.Configuration.GetConnectionString("TourPlannerDb")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
