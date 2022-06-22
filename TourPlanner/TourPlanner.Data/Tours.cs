904f4f587c052bf187bf8a1327eb4114ba4dc71c
Linus Dehner <if21b132@technikum-wien.at>
2022-06-22 22:01:56 +0200

    Added project for entity framework db

---
+++ TourPlanner/TourPlanner.Data/Tours.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Data
+{
+    public class Tours
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid TourId { get; set; }
+        public string Name { get; set; }
+        [MaxLength(30)]
+        public string Description { get; set; }
+
+    }
+}
---
+++ TourPlanner/TourPlanner.Data/ToursDataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Data
+{
+    public class ToursDataContext : DbContext
+    {
+        public ToursDataContext(DbContextOptions<ToursDataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+        public DbSet<Tours> Tours { get; set; }
+    }
+}
