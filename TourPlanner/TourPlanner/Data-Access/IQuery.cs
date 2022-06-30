89d71dae852ad2b0e136f3c85a84c0620d357c28
dbruckner01-tgm <dariobruckner@gmail.com>
2022-06-30 10:57:08 +0200

    created Data-Access framework

---
+++ TourPlanner/TourPlanner/Data-Access/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tour_planner.Business;
+
+namespace Tour_planner.Data_Access
+{
+    public interface IQuery
+    {
+        public Task<Tourlist> GetTours();
+
+    }
+}
---
+++ TourPlanner/TourPlanner/Data-Access/Requests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Tour_planner.Business;
+
+namespace Tour_planner.Data_Access
+{
+    public class Requests : IQuery
+    {
+
+        private const string Url = "https://localhost:7180/api";
+        public async Task<Tourlist> GetTours()
+        {
+            using var client = new HttpClient();
+
+            var response = await client.GetStringAsync(Url);
+            Tourlist? tourlist = JsonSerializer.Deserialize<Tourlist>(response.ToString());
+            return tourlist;
+        }
+    }
+}
