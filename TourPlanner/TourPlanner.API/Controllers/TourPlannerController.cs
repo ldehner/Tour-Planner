40393ab185d09e5e495b776e256ae016b7c06abd
Linus Dehner <if21b132@technikum-wien.at>
2022-07-06 23:03:51 +0200

    documented controller

--- TourPlanner/TourPlanner.API/Controllers/TourPlannerController.cs
+++ TourPlanner/TourPlanner.API/Controllers/TourPlannerController.cs
@@ -15,6 +15,9 @@ using Newtonsoft.Json.Converters;
 
 namespace TourPlannerAPI.Controllers
 {
+    /// <summary>
+    /// API Controller for the tour planner
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TourPlannerController : ControllerBase
@@ -24,6 +27,13 @@ namespace TourPlannerAPI.Controllers
         private readonly IMapQuestManager _mapQuestManager;
         private readonly ILogger<TourPlannerController> _logger;
 
+        /// <summary>
+        /// Tour Planner Controller Constructor
+        /// </summary>
+        /// <param name="logger">Log4Net logger</param>
+        /// <param name="tourManager">the tour manager</param>
+        /// <param name="tourLogManager">the log manager</param>
+        /// <param name="mapQuestManager">the mapquest manager</param>
         public TourPlannerController(ILogger<TourPlannerController> logger, ITourManager tourManager, ITourLogManager tourLogManager, IMapQuestManager mapQuestManager)
         {
             _logger = logger;
@@ -31,20 +41,28 @@ namespace TourPlannerAPI.Controllers
             _tourLogManager = tourLogManager;
             _mapQuestManager = mapQuestManager;
         }
+
+        /// <summary>
+        /// Gets all tours
+        /// </summary>
+        /// <response code="200">Success - All existing tours returned</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>All existing tours</returns>
         [HttpGet("GetTours")]
         public async Task<ActionResult<List<PresentationTour>>> Get()
         {
             _logger.LogInformation("API Request - Get all tours");
             return Ok(await _tourManager.GetToursAsync());
         }
+
         /// <summary>
-        /// 
+        /// Gets tour with specific tourId
         /// </summary>
         /// <param name="TourId"></param>
-        /// <response code="200">Tour returned</response>
-        /// <response code="400">Tour could not be found</response>
+        /// <response code="200">Success - Tour returned</response>
+        /// <response code="404">Tour could not be found</response>
         /// <response code="500">Oops! Problem on our end</response>
-        /// <returns></returns>
+        /// <returns>The searched tour</returns>
         [HttpGet("GetTours/{TourId}")]
         public async Task<ActionResult<PresentationTour>> Get(Guid TourId)
         {
@@ -56,11 +74,18 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
                 return NotFound("Tour not found");
             }
         }
 
+        /// <summary>
+        /// Adds a new tour
+        /// </summary>
+        /// <param name="tour">the tour</param>
+        /// <response code="200">Success - Tour created</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the created tour</returns>
         [HttpPost("AddTour")]
         public async Task<ActionResult<PresentationTour>> AddTour(SimpleTour tour)
         {
@@ -71,7 +96,15 @@ namespace TourPlannerAPI.Controllers
             await _mapQuestManager.SaveMapAsync(presentation.TourId, map);
             return Ok(presentation);
         }
-        //DeleteTour
+
+        /// <summary>
+        /// Deletes a tour with a given id
+        /// </summary>
+        /// <param name="TourId">The id of the tour</param>
+        /// <response code="200">Success - Tour deleted</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>all tours</returns>
         [HttpDelete("DeleteTour/{TourId}")]
         public async Task<ActionResult<List<PresentationTour>>> DeleteTour(Guid TourId)
         {
@@ -83,11 +116,20 @@ namespace TourPlannerAPI.Controllers
                 return Ok(result);
             }catch(TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
             }
         }
-        //EditTour
+
+        /// <summary>
+        /// Updates a tour
+        /// </summary>
+        /// <param name="TourId">id of the tour</param>
+        /// <param name="request">the new tour</param>
+        /// <response code="200">Success - Tour updated</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the updated tour</returns>
         [HttpPut("UpdateTour/{TourId}")]
         public async Task<ActionResult<PresentationTour>> UpdateTour(Guid TourId, SimpleTour request)
         {
@@ -103,11 +145,20 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
             }
         }
-        //AddLog
+
+        /// <summary>
+        /// Adds a log to a tour
+        /// </summary>
+        /// <param name="TourId">the id of the tour</param>
+        /// <param name="log">the log</param>
+        /// <response code="200">Success - Tour updated</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the tour including the log</returns>
         [HttpPost("AddLog/{TourId}")]
         public async Task<ActionResult<PresentationTour>> AddLog(Guid TourId, SimpleLog log)
         {
@@ -119,12 +170,19 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
             }
         }
-        
-        //GetLogs
+
+        /// <summary>
+        /// Gets all logs of tour
+        /// </summary>
+        /// <param name="TourId">the id of the tour</param>
+        /// <response code="200">Success - Tour found</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>All the logs of the tour</returns>
         [HttpGet("GetLogs/{TourId}")]
         public async Task<ActionResult<List<PresentationLog>>> GetLogs(Guid TourId)
         {
@@ -136,12 +194,21 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
             }
 
         }
-        //GetLogs/{LogId}
+
+        /// <summary>
+        /// Gets a specific log of a tour
+        /// </summary>
+        /// <param name="TourId">the id of the tour</param>
+        /// <param name="LogId">the id of the log</param>
+        /// <response code="200">Success - Log found</response>
+        /// <response code="404">Tour or Log could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the log</returns>
         [HttpGet("GetLogs/{TourId}/{LogId}")]
         public async Task<ActionResult<PresentationLog>> GetLog(Guid TourId, Guid LogId)
         {
@@ -153,16 +220,25 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
             }
             catch (LogNotFoundException)
             {
-                _logger.LogInformation("Requested log " + LogId.ToString() + " not found in tour " + TourId.ToString());
-                return BadRequest("Log not found");
+                _logger.LogError("Requested log " + LogId.ToString() + " not found in tour " + TourId.ToString());
+                return NotFound("Log not found");
             }
         }
-        //DeleteLog
+
+        /// <summary>
+        /// Deletes log of tour
+        /// </summary>
+        /// <param name="TourId">the id of the tour</param>
+        /// <param name="LogId">the id of the log</param>
+        /// <response code="200">Success - Log deleted</response>
+        /// <response code="404">Tour or Log could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>all the existing logs</returns>
         [HttpDelete("DeleteLog/{TourId}/{LogId}")]
         public async Task<ActionResult<List<PresentationLog>>> DeleteLog(Guid TourId, Guid LogId)
         {
@@ -175,29 +251,69 @@ namespace TourPlannerAPI.Controllers
             catch (TourNotFoundException)
             {
                 _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                return NotFound("Tour not found");
             }
             catch (LogNotFoundException)
             {
-                _logger.LogInformation("Requested log " + LogId.ToString() + " not found in tour " + TourId.ToString());
-                return BadRequest("Log not found");
+                _logger.LogError("Requested log " + LogId.ToString() + " not found in tour " + TourId.ToString());
+                return NotFound("Log not found");
             }
         }
 
+        /// <summary>
+        ///  Gets the map of a round as byte array
+        /// </summary>
+        /// <param name="TourId">the tour id</param>
+        /// <response code="200">Success - Map found</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the map</returns>
         [HttpGet("GetMap/{TourId}")]
         public async Task<ActionResult<byte[]>> GetMap(Guid TourId)
         {
             _logger.LogInformation("API Request - Get map of tour " + TourId.ToString());
-            return Ok(await _mapQuestManager.LoadMapAsync(TourId));
+            try
+            {
+                var result = await _mapQuestManager.LoadMapAsync(TourId);
+                return Ok(result);
+            }
+            catch (TourNotFoundException)
+            {
+                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
+            }
         }
 
+        /// <summary>
+        /// Generates report of tour
+        /// </summary>
+        /// <param name="TourId">the tour id</param>
+        /// <response code="200">Success - Report generated</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the report</returns>
         [HttpGet("TourReport/{TourId}")]
         public async Task<ActionResult<byte[]>> RouteReport(Guid TourId)
         {
             _logger.LogInformation("API Request - Generate report of tour " + TourId.ToString());
-            return Ok(await _tourManager.GenerateTourReportAsync(TourId));
+            try
+            {
+                var result = await _tourManager.GenerateTourReportAsync(TourId);
+                return Ok(result);
+            }
+            catch (TourNotFoundException)
+            {
+                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
+            }
         }
 
+        /// <summary>
+        /// Generates overview report of all tours
+        /// </summary>
+        /// <response code="200">Success - Overview report generated</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the report</returns>
         [HttpGet("TourOverviewReport")]
         public async Task<ActionResult<byte[]>> TourOverviewReport()
         {
@@ -205,6 +321,14 @@ namespace TourPlannerAPI.Controllers
             return Ok(await _tourManager.GenerateTourOverviewAsync());
         }
 
+        /// <summary>
+        /// Searches for a term
+        /// </summary>
+        /// <param name="searchTerm">the search term</param>
+        /// <response code="200">Success - Found matches</response>
+        /// /// <response code="404">No match found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>all the matching tours</returns>
         [HttpGet("search/{searchTerm}")]
         public async Task<ActionResult<List<PresentationTour>>> SearchTours(string searchTerm)
         {
@@ -212,6 +336,14 @@ namespace TourPlannerAPI.Controllers
             return Ok(await _tourManager.SearchAsync(searchTerm.ToUpper()));
         }
 
+        /// <summary>
+        /// Exports a tour
+        /// </summary>
+        /// <param name="TourId">the tour id</param>
+        /// <response code="200">Success - Tour exported</response>
+        /// <response code="404">Tour could not be found</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the tour</returns>
         [HttpGet("export/{TourId}")]
         public async Task<ActionResult<string>> ExportTour(Guid TourId)
         {
@@ -223,11 +355,20 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourNotFoundException)
             {
-                _logger.LogInformation("Requested tour " + TourId.ToString() + " not found");
-                return BadRequest("Tour not found");
+                _logger.LogError("Requested tour " + TourId.ToString() + " not found");
+                return NotFound("Tour not found");
             }
             
         }
+
+        /// <summary>
+        /// imports a tour
+        /// </summary>
+        /// <param name="tour">the tour id</param>
+        /// <response code="200">Success - Tour imported</response>
+        /// <response code="409">Tour already exists</response>
+        /// <response code="500">Oops! Problem on our end</response>
+        /// <returns>the imported tour</returns>
         [HttpPost("import")]
         public async Task<ActionResult<PresentationTour>> ImportTour(PresentationTour tour)
         {
@@ -244,12 +385,9 @@ namespace TourPlannerAPI.Controllers
             }
             catch (TourAlreadyExistsException)
             {
-                _logger.LogInformation("Tour " + tour.TourId.ToString() + " already exists");
-                return BadRequest("Tour already exists");
+                _logger.LogError("Tour " + tour.TourId.ToString() + " already exists");
+                return Conflict("Tour already exists");
             }
         }
-
-
-
     }
 }
